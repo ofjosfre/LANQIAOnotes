cb45e1d201dde755f373e3ee37930d02fffb1cdc
ofjosfre <101401012+ofjosfre@users.noreply.github.com>
2022-03-13 17:14:05 +0800

    Add files via upload

---
+++ 初级/5.1独立按键2/5.1.c
@@ -0,0 +1,145 @@
+#include "reg52.h"
+
+sbit HC138_A=P2^5;
+sbit HC138_B=P2^6;
+sbit HC138_C=P2^7;
+sbit S7=P3^0;
+sbit S6=P3^1;
+sbit S5=P3^2;
+sbit S4=P3^3;
+
+sbit L1=P0^0;
+sbit L2=P0^1;
+sbit L3=P0^2;
+sbit L4=P0^3;
+sbit L5=P0^4;
+sbit L6=P0^5;
+
+
+
+void select(unsigned char m)
+{
+	switch (m)
+	{
+	case 4:
+  HC138_C=1;
+  HC138_B=0;
+  HC138_A=0;
+  break;
+  case 5:
+  HC138_C=1;
+  HC138_B=0;
+  HC138_A=1;
+  break;
+  case 6:
+  HC138_C=1;
+  HC138_B=1;
+  HC138_A=0;
+  break;
+  case 7:
+  HC138_C=1;
+  HC138_B=1;
+  HC138_A=1;
+  break;
+	}	
+}
+
+void initial()
+{
+select(5);
+P0=0x00;
+}
+
+
+void delay1(unsigned int t)
+{
+	while(t--);
+}
+
+
+unsigned char statek=0;
+
+
+void scankeys_alone()
+{
+if(S7==0)
+	{
+		delay1(100);
+		if(S7==0)
+		{
+			if(statek==0)
+			{
+		   L1=0;
+		   statek=1;
+		  }
+			else if(statek==1)
+			{
+		    L1=1;
+				statek=0;
+		  }
+			while(S7==0);
+	  }
+  }
+
+if (S6==0)
+	{
+			delay1(100);
+   if(S6==0)
+		  {
+				if(statek==0)
+				{
+					L2=0;
+					statek=2;
+			  }
+				else if(statek==2)
+				 {
+						L2=1;
+						statek=0;
+			   } 
+		     while(S6==0);
+		  } 			
+	}	
+	if (S5==0)
+		{
+			delay1(100);
+   if(S5==0)
+		  {
+				if(statek==1)
+				{
+					L3=0;
+					while(S5==0);
+					L3=1;
+				}
+				else if(statek==2)
+				{
+					L5=0;
+					while(S5==0);
+					L5=1;
+				}
+		  } 	
+		}			
+	 if(S4==0)
+		  {
+				if(statek==1)
+				{
+					L4=0;
+					while(S4==0);
+					L4=1;
+				}
+				else if(statek==2)
+				{
+					L6=0;
+					while(S4==0);
+					L6=1;
+				}
+		  } 			
+}
+
+void main()
+{
+select(4);
+	while(1)
+	{
+		scankeys_alone();
+	}
+}
